85f1cde760b0e8f61f0c50f0410e1fcf0ed513d0
Dusan Sandic <dusan.sandic@student.tugraz.at>
2026-07-03 21:37:01 +0200

    Updated nvim config related to Telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -87,8 +87,8 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = "<space>"
-vim.g.maplocalleader = " "
+vim.g.mapleader = " "
+-- vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
@@ -1011,3 +1011,7 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.smartindent = true
+
+-- Spell checking
+vim.opt.spelllang = "en_us"
+vim.opt.spell = true
